1de01e9a88451f8119aff750547af15ba1af612e
aacebo <aacebo@gmail.com>
2024-06-11 08:31:11 -0400

    iniciado  la aplicacion

---
+++ app/Models/Usuario.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    public $timestamps = false;
+    protected $table = "usuarios";
+    protected $primaryKey = "id";
+
+    protected $fillable =['nombre','email','contraseña'];
+    use HasFactory;
+}
